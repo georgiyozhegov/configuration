4a71fdb8e0a0ac43df8564cbdb8a3123d50a2ff9
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-08-15 12:31:00 +0400

    Sponge plugin for Fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    theme_gruvbox dark soft
-    set -g fish_key_bindings fish_vi_key_bindings
-    set -gx EDITOR nvim
+      theme_gruvbox dark soft
+      set -g fish_key_bindings fish_vi_key_bindings
+      set -gx EDITOR nvim
 end
