66115ba612a24dfff1a7c35f143c7ceb9ff760c6
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-08-26 18:05:23 +0400

    Fish history-cleaning settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
       theme_gruvbox dark soft
       set -g fish_key_bindings fish_vi_key_bindings
+      set sponge_allow_previosly_successful true
+      set sponge_successful_exit_codes 0 1 127
       set -gx EDITOR nvim
 end
