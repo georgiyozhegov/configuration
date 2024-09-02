82bae63cc0a3b0daaeabb6007fcbf6570e3cb262
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-09-02 17:55:20 +0400

    Increased `colorcolumn` in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,7 @@ vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
 vim.opt.cursorline = true
+vim.opt.colorcolumn = "94"
 
 vim.keymap.set("n", "wh", "<C-w>h")
 vim.keymap.set("n", "wj", "<C-w>j")
